dc9d1b780a01ff0e4e28c50cb0b3a147d3ca11f4
Kisoong Jang <kisjang@kr.ibm.com>
2021-08-31 19:31:09 +0900

    Develop a DAG to call BashOperator using shell

--- src/dag/shell_test.py
+++ src/dag/shell_test.py
@@ -2,17 +2,27 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+import pendulum
+
+local_tz = pendulum.timezone("Asia/Seoul")
+
+default_args= {
+    'start_date': days_ago(1),
+    'retries': 0,
+    'catchup': False,
+    'retry_delay': timedelta(minutes=5),
+}
 
 dag = DAG(
-	'shell_test', 
-	schedule_interval="0 1 * * *", 
-	start_date=datetime(2021, 8, 31), 
-	catchup=False,
+        'shell_test', 
+        default_args=default_args, 
+        schedule_interval="@daily",
 )
 
 # Task 1
 dummy_task = DummyOperator(task_id='dummy_task', retries=1, dag=dag)
 # Task 2
-bash_task = BashOperator(task_id='bash_task', bash_command="$AIRFLOW_HOME/dags/command.sh ", xcom_push=True, dag=dag)
+bash_task = BashOperator(task_id='bash_task', bash_command="$AIRFLOW_HOME/dags/command.sh ", dag=dag)
 
 dummy_task >> bash_task
